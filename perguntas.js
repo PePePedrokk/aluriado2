54181f53f1234669bc49c361cde7e1beb43c58e4
PePePedrokk <141240983+PePePedrokk@users.noreply.github.com>
2024-11-21 17:39:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,50 @@
+
+criaCartao(
+    'Esportes',
+    'Qual clube venceu a primeira edição da UEFA Champions League?',
+    'Real Madrid(1956)'
+)
+
+criaCartao(
+    'Esportes', 
+    'Quem é o maior artilheiro da história da Champions League?',
+    'Cristiano Ronaldo (140)'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual jogador marcou o gol mais rápido em uma final de Champions League?',
+    'Paolo Maldini (AC Milan, 2005)'
+)
+
+criaCartao(
+    'Esportes',
+    'Quem são os maiores artilheiros de uma unica edição?',
+    'Cristiano Ronaldo (17), Cristiano Ronaldo (16), Robert Lewandoski(15)'
+)
+
+
+criaCartao(
+    'Esportes',
+    'Qual foi o primeiro clube inglês a vencer a Champions',
+    'Manchester United (1968)'
+)
+
+
+criaCartao(
+    'Esportes',
+    'Quem é o treinador com mais títulos de Champions League?',
+    'Carlo Ancelotti (4 títulos)'
+)
+
+criaCartao(
+    'Esportes',
+    'Quais times se enfrentaram na final de 2019?',
+    'Tottenham X Liverpool'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual time ganhou a champions de 2020?',
+    'Bayern De Munique'
+)
